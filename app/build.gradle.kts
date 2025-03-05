dc21023f9455c08fc975b50bb5436019c3356d8a
MiddleShamil <dmitriy_abdullaev@bk.ru>
2025-03-05 16:56:01 +0300

    add ksp, room, viewbinding from android broadcast and splash API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,12 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.services)
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
-}
-
-kapt {
-    correctErrorTypes = true
 }
 
 android {
@@ -46,18 +42,33 @@ android {
 }
 
 dependencies {
+    val room_version = "2.6.1"
 
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
+    // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 
+    // Hilt
     implementation ("com.google.dagger:hilt-android:2.51.1")
-    kapt ("com.google.dagger:hilt-compiler:2.51.1")
+    ksp ("com.google.dagger:hilt-compiler:2.51.1")
+
+    // ViewBinding
+    implementation("dev.androidbroadcast.vbpd:vbpd:2.0.4")
+
+    // Splash API
+    implementation("androidx.core:core-splashscreen:1.0.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
