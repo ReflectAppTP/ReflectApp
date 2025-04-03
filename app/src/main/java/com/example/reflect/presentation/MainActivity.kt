875539ca77eb078b6dc122a4bda43fcd1798063a
Dmitriy Abdullaev <dmitriy_abdullaev@bk.ru>
2025-04-03 14:47:19 +0300

    fix(TP-169): fix app icon and navigation in mainActivity

--- app/src/main/java/com/example/reflect/presentation/MainActivity.kt
+++ app/src/main/java/com/example/reflect/presentation/MainActivity.kt
@@ -2,20 +2,11 @@ package com.example.reflect.presentation
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.reflect.R
 import com.example.reflect.databinding.ActivityMainBinding
-import com.example.reflect.presentation.friends.fragment.FriendsFragment
-import com.example.reflect.presentation.profile.fragment.ProfileFragment
-import com.example.reflect.presentation.records.fragment.RecordsFragment
-import com.example.reflect.presentation.statistics.fragment.StatisticsFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,20 +16,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // TODO: обернуть в отдельную функцию
-        val bottomNavBar = binding.bottomNavigationBar
+        val bottomNavBar = binding.bottomNavBar
         val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
         val navController = navHostFragment.navController
         bottomNavBar.setupWithNavController(navController)
-        bottomNavBar.setOnItemSelectedListener { item ->
-            when(item.itemId) {
+        bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.recordsFragment -> {
+                    navController.navigate(it.itemId)
+                    true
+                }
+                R.id.statisticsFragment -> {
+                    navController.navigate(it.itemId)
+                    true
+                }
                 R.id.add_record -> {
                     Toast.makeText(this, "Мяу мяу...", Toast.LENGTH_SHORT).show()
                     false
                 }
-                else -> {
-                    navController.navigate(item.itemId)
+                R.id.friendsFragment -> {
+                    navController.navigate(it.itemId)
                     true
                 }
+                R.id.profileFragment -> {
+                    navController.navigate(it.itemId)
+                    true
+                }
+                else -> false
             }
         }
     }
