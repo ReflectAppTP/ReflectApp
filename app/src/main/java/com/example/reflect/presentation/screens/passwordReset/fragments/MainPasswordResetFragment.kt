55dd4b4e6a324ccc8663ad2519d483dc2fc61505
Dmitriy Abdullaev <dmitriy_abdullaev@bk.ru>
2025-04-07 20:05:33 +0300

    fix(TP-199): fix navigation on resetPassword screens

--- app/src/main/java/com/example/reflect/presentation/screens/passwordReset/fragments/MainPasswordResetFragment.kt
+++ app/src/main/java/com/example/reflect/presentation/screens/passwordReset/fragments/MainPasswordResetFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.reflect.R
 import com.example.reflect.databinding.FragmentMainPasswordResetBinding
 import com.example.reflect.presentation.screens.passwordReset.viewmodel.ViewModelPasswordReset
 
@@ -20,8 +23,19 @@ class MainPasswordResetFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentMainPasswordResetBinding.inflate(inflater, container, false)
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.resetPasswordBackArrow.setOnClickListener {
+            if (binding.mainFragmentContainer.findNavController().currentDestination?.label.toString() == resources.getResourceEntryName(R.layout.fragment_password_reset_email)) {
+                findNavController().popBackStack()
+            } else {
+                binding.mainFragmentContainer.findNavController().popBackStack()
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/reflect/presentation/screens/passwordReset/fragments/PasswordResetNewPasswordFragment.kt
+++ app/src/main/java/com/example/reflect/presentation/screens/passwordReset/fragments/PasswordResetNewPasswordFragment.kt
@@ -1,11 +1,15 @@
 package com.example.reflect.presentation.screens.passwordReset.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.reflect.R
 import com.example.reflect.databinding.FragmentPasswordResetNewPasswordBinding
 import com.example.reflect.presentation.screens.passwordReset.viewmodel.ViewModelPasswordReset
 
@@ -21,7 +25,18 @@ class PasswordResetNewPasswordFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentPasswordResetNewPasswordBinding.inflate(inflater, container, false)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(requireActivity(), R.id.mainFragmentContainer)
+
+        binding.resetPasswordChangePasswordButton.setOnClickListener {
+            navController.navigate(R.id.action_passwordResetNewPasswordFragment_to_mainFragment)
+//            requireParentFragment().findNavController().navigate(R.id.action_passwordResetNewPasswordFragment_to_mainFragment)
+        }
     }
 
     override fun onDestroyView() {
