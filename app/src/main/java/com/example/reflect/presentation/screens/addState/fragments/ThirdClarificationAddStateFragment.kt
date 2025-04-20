06015c27c72817110575ed11dec03d7a87d30386
Dmitriy Abdullaev <dmitriy_abdullaev@bk.ru>
2025-04-20 11:01:28 +0300

    fix(TP-258): fix bug on thirdClarificationFragment on editTextField

--- app/src/main/java/com/example/reflect/presentation/screens/addState/fragments/ThirdClarificationAddStateFragment.kt
+++ app/src/main/java/com/example/reflect/presentation/screens/addState/fragments/ThirdClarificationAddStateFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.activityViewModels
 import com.example.reflect.databinding.FragmentThirdClarificationAddStateBinding
@@ -83,12 +81,6 @@ class ThirdClarificationAddStateFragment : Fragment() {
                 Utils.toast(requireContext())
                 // TODO Просто bruh!
                 (parentFragment?.parentFragment as BottomSheetDialogFragment).dismiss()
-            }
-            addStateThirdClarificationTextInputField.setOnEditorActionListener { _, actionId, _ ->
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    addStateThirdClarificationNextButton.performClick()
-                }
-                true
             }
         }
     }
