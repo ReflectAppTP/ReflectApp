9cbf46b0ad73fd4ac49641bedd28a07038407582
Dmitriy Abdullaev <dmitriy_abdullaev@bk.ru>
2025-05-15 16:22:51 +0300

    feature(TP-479): add ai screen

--- app/src/main/java/com/example/reflect/presentation/screens/friends/fragment/FriendsFragment.kt
+++ app/src/main/java/com/example/reflect/presentation/screens/friends/fragment/FriendsFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.reflect.databinding.FragmentFriendsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FriendsFragment : Fragment() {
 
     private var _binding: FragmentFriendsBinding? = null
